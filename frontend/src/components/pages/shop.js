094d509fd36af71e9d68dfaf97fc060a99c4154d
neilraphaelramos <neilraphael.ramos4@gmail.com>
2025-05-20 13:26:20 +0800

    Add Design of Services, Products and About

--- frontend/src/components/pages/shop.js
+++ frontend/src/components/pages/shop.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import { IoPerson } from "react-icons/io5";
-import { IoIosArrowBack, IoIosArrowForward  } from "react-icons/io";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { PiChatsDuotone } from "react-icons/pi";
 
 
@@ -10,15 +10,15 @@ function ShopPage() {
     const page4Ref = useRef(null);
 
     const scrollToPage2 = () => {
-        page2Ref.current?.scrollIntoView({ behavior: 'smooth'});
+        page2Ref.current?.scrollIntoView({ behavior: 'smooth' });
     };
 
     const scrollToPage3 = () => {
-        page3Ref.current?.scrollIntoView({ behavior: 'smooth'});
+        page3Ref.current?.scrollIntoView({ behavior: 'smooth' });
     };
 
     const scrollToPage4 = () => {
-        page4Ref.current?.scrollIntoView({ behavior: 'smooth'});
+        page4Ref.current?.scrollIntoView({ behavior: 'smooth' });
     };
 
     return (
@@ -61,8 +61,8 @@ function ShopPage() {
                             Subheading with description of your shopping site
                         </p>
                     </div>
-                    <IoIosArrowBack className='arrow-back-btn'/>
-                    <IoIosArrowForward className='arrow-forward-btn'/>
+                    <IoIosArrowBack className='arrow-back-btn' />
+                    <IoIosArrowForward className='arrow-forward-btn' />
                     <div className='btn-group-track-book'>
                         <button className='btn-book'>
                             Book an Appointment
@@ -72,7 +72,7 @@ function ShopPage() {
                         </button>
                     </div>
                     <button className='btn-chat'>
-                        <PiChatsDuotone className='icon-chat'/>
+                        <PiChatsDuotone className='icon-chat' />
                         Chat with us
                     </button>
                 </div>
@@ -82,18 +82,102 @@ function ShopPage() {
                     Our Services
                 </h1>
                 <div className='services-container'>
-                    
+                    <div className='items-container'>
+                        <div className='card-item'>
+                            <img alt='img' src='Consultation.png' />
+                            <h1 className='title-card'>
+                                CONSULTATION
+                            </h1>
+                            <p className='card-description'>
+                                Rivera Veterinary Clinic provides
+                                thorough consultations to assess
+                                and address your pet's health needs.
+                            </p>
+                        </div>
+                        <div className='card-item'>
+                            <img alt='img' src='Grooming.png' />
+                            <h1 className='title-card'>
+                                GROOMING
+                            </h1>
+                            <p className='card-description'>
+                                In addition to medical care,
+                                we offer professional grooming
+                                services to keep your pets clean
+                                and healthy.
+                            </p>
+                        </div>
+                        <div className='card-item'>
+                            <img alt='img' src='Vaccination.png' />
+                            <h1 className='title-card'>
+                                VACCINATION
+                            </h1>
+                            <p className='card-description'>
+                                Our clinic is equipped to perform
+                                various surgical procedures with
+                                utmost care and precision.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className='page-3' ref={page3Ref}>
                 <h1 className='title-text-3'>
                     Pet Health Products
                 </h1>
+                <div className='selection-products'>
+                    <a href='n' className='link-product'>
+                        Treats
+                    </a>
+                    <a href='n' className='link-product'>
+                        Grooming Products
+                    </a>
+                    <a href='n' className='link-product'>
+                        Vitamins
+                    </a>
+                </div>
+                <div className='products-container'>
+                    <div className='items-container'>
+                        <div className='card-item'>
+                            <img alt='img' src='Consultation.png' />
+                            <h1 className='title-card'>
+                                CONSULTATION
+                            </h1>
+                        </div>
+                        <div className='card-item'>
+                            <img alt='img' src='Grooming.png' />
+                            <h1 className='title-card'>
+                                GROOMING
+                            </h1>
+                        </div>
+                        <div className='card-item'>
+                            <img alt='img' src='Vaccination.png' />
+                            <h1 className='title-card'>
+                                VACCINATION
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+                <IoIosArrowForward className='arrow-forward-btn' />
             </div>
             <div className='page-4' ref={page4Ref}>
-                <h1 className='title-text-4'>
-                    ABOUT US
-                </h1>
+                <div className='about-description'>
+                    <h1 className='title-text-4'>
+                        ABOUT US
+                    </h1>
+                    <p className='description-bout'>
+                        Rivera Veterinary Clinic and Grooming Services 
+                        is a trusted pet care clinic in Lolomboy, Bocaue, 
+                        Bulacan, offering comprehensive services for dogs 
+                        and cats including consultations, blood tests, surgery, 
+                        confinement, vaccination, deworming, grooming, and home 
+                        services. With a small and dedicated team, they provide 
+                        quality veterinary care with compassion.
+                    </p>
+                </div>
+                <div className='image-display'>
+                    <img alt='image0-background' src='BG Image.png' className='background-image'/>
+                    <img alt='image0-background' src='Image About.png'className='image-about'/>
+                </div>
             </div>
         </div>
     )
